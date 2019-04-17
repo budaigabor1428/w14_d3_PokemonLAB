36cafa923be7a01c2bae00d8e56b701073e1e532
Gabor Budai <budaigabor1428@gmail.com>
2019-04-17 15:31:39 +0100

    abilities added

--- src/Components/PokemonDetail.js
+++ src/Components/PokemonDetail.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
   const PokemonDetail = (props) => {
-
     if(!props.pokemon) return null;
+    const abilities = props.pokemon.abilities.map(ability => {return <p>{ability.ability.name}</p>})
+
+
+
     return(
       <div>
       <p>id: {props.pokemon.id}</p>
-      <p>abilities: {props.pokemon.abilities[0].ability.name}</p>
+      {abilities}
+      <p><img src={props.pokemon.sprites.back_default} width = "1500px"/></p>
       </div>
 
 
